f94b2d02802aa8f80b6e73365083b27bf6d38063
Jack Donaldson <jackdonaldson005@gmail.com>
2023-03-26 18:25:29 +1100

    Add Linting & PR Check (#1)

---
+++ src/webpack/webpack.dev.js
@@ -0,0 +1,32 @@
+const path = require("path")
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.ts",
+  devtool: "eval-source-map",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "../../dist/client"),
+    },
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "../../dist/client"),
+  },
+  performance: {
+    hints: false,
+  },
+}
