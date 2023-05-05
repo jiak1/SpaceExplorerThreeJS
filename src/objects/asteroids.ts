a9803cf02402a950c1e8b1eadb611f3cc6452635
Michael Ning <crown.d@hotmail.com.au>
2023-05-05 14:29:48 +1000

    Setup Asteroid properly working. Added feature to setup Asteroids as clusters.

--- src/objects/asteroids.ts
+++ src/objects/asteroids.ts
@@ -1,36 +1,57 @@
 import * as THREE from "three"
 import { scene } from "../renderer/renderer"
+//----------------------------------------------------------------------------------------------------------------------------------------------------------
 
-//VERY INCOMPLETE. JUST A COPY OF PLANETS//
+// Create a new instance of the object you want to spawn
+const geometry = new THREE.BoxGeometry(10, 10, 10);
+const material = new THREE.MeshBasicMaterial({ color: 0x808080 });
+const object = new THREE.Mesh(geometry, material);
 
-const asteroids: THREE.Mesh[] = []
+// Define the number of clusters and the number of objects per cluster
+const numClusters = 5;
+const objectsPerCluster = 10;
 
-const asteroidShader = {
-  uniforms: {
-    time: { type: "f", value: 0.0 },
-    scale: { type: "f", value: 0.45 },
-    amplitude: { type: "f", value: 1 },
-    octaves: { type: "i", value: 6 },
-  },
+// Define the size of the area in which the clusters will be spawned
+const clusterAreaSize = 500;
+
+// Create a new group to hold the objects
+const group = new THREE.Group();
+
+// Loop through the number of clusters
+for (let i = 0; i < numClusters; i++) {
+  // Generate a random position for the cluster within the cluster area
+  const clusterX = Math.random() * clusterAreaSize - clusterAreaSize / 2;
+  const clusterY = Math.random() * clusterAreaSize - clusterAreaSize / 2;
+  const clusterZ = Math.random() * clusterAreaSize - clusterAreaSize / 2;
+
+  // Create a new group to hold the objects in this cluster
+  const clusterGroup = new THREE.Group();
+
+  // Loop through the number of objects per cluster
+  for (let j = 0; j < objectsPerCluster; j++) {
+    // Generate a random position for the object within the cluster
+    const objectX = Math.random() * 100 - 5;
+    const objectY = Math.random() * 100 - 5;
+    const objectZ = Math.random() * 100 - 5;
+
+    // Create a new instance of the object
+    const instance = object.clone();
+
+    // Set the position of the object relative to the position of the cluster
+    instance.position.set(clusterX + objectX, clusterY + objectY, clusterZ + objectZ);
+
+    // Add the object to the cluster group
+    clusterGroup.add(instance);
+  }
+
+  // Add the cluster group to the main group
+  group.add(clusterGroup);
 }
 
-const createAsteroid = () => {
-  const geometry: THREE.BoxGeometry = new THREE.BoxGeometry(10, 10, 10)
-
-  const asteroidMaterial = new THREE.ShaderMaterial({
-    uniforms: asteroidShader.uniforms,
-    vertexShader: asteroidShader.vertexShader,
-    fragmentShader: asteroidShader.fragmentShader,
-  })
-
-  return new THREE.Mesh(geometry, asteroidMaterial)
-}
+//----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 const setupAsteroids = () => {
-  const asteroid = createAsteroid()
-  scene.add(asteroid)
-
-  asteroids.push(asteroid)
+  scene.add(group)
 }
 
-export { setupAsteroids, asteroids }
+export { setupAsteroids, group }
--- src/objects/objects.ts
+++ src/objects/objects.ts
@@ -1,11 +1,13 @@
 import { setupPlanets } from "./planet"
 import { setupShip } from "./ship"
 import { setupSkybox } from "./skybox"
+import { setupAsteroids } from "./asteroids"
 
 const setupObjects = () => {
   setupPlanets()
   setupShip()
   setupSkybox()
+  setupAsteroids()
 }
 
 export { setupObjects }
