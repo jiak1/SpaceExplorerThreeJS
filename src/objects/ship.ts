062c6ef77f6a4df2e72a614a46271b083c68d8a1
Joshua Karadaghian <joshuak188@gmail.com>
2023-05-14 19:47:00 +1000

    Movement of code to setupShip

--- src/objects/ship.ts
+++ src/objects/ship.ts
@@ -5,25 +5,25 @@ import { scene } from "../renderer/renderer"
 const model = "textures/spaceship.glb"
 let ship: THREE.Group
 
-const loader = new GLTFLoader()
-loader.load(
-  model,
-  (gltf) => {
-    gltf.scene.position.set(0, 25, 0)
-    ship = gltf.scene
-    scene.add(gltf.scene)
-    const shipMesh = ship.children[0] as THREE.Mesh
-    shipMesh.material = new THREE.MeshBasicMaterial({ color: "#ADD8E6" })
-    shipMesh.rotateZ(Math.PI)
-  },
-  (xhr) => {
-    console.log(`${(xhr.loaded / xhr.total) * 100}% loaded`)
-  },
-  (error) => {
-    console.log("An error happened")
-  }
-)
-
-const setupShip = () => { }
+const setupShip = () => {
+  const loader = new GLTFLoader()
+  loader.load(
+    model,
+    (gltf) => {
+      gltf.scene.position.set(0, 25, 0)
+      ship = gltf.scene
+      scene.add(gltf.scene)
+      const shipMesh = ship.children[0] as THREE.Mesh
+      shipMesh.material = new THREE.MeshBasicMaterial({ color: "#ADD8E6" })
+      shipMesh.rotateZ(Math.PI)
+    },
+    (xhr) => {
+      console.log(`${(xhr.loaded / xhr.total) * 100}% loaded`)
+    },
+    (error) => {
+      console.log("An error happened")
+    }
+  )
+}
 
 export { setupShip, ship }
