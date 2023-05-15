877c32518f9c5d66f8c868c195009a54d96871a6
Michael Ning <crown.d@hotmail.com.au>
2023-05-15 14:45:44 +1000

    Revert "Revert "Added Asteroids""
    This reverts commit cda2a8894fff8647e386d7e9a22aa10cc51861cb.

--- src/objects/asteroids.ts
+++ src/objects/asteroids.ts
@@ -20,7 +20,7 @@ const vertexShader = `
 
     gl_Position = projectionMatrix * modelViewMatrix * vec4(displaced, 1.0);
   }
-`;
+`
 
 const fragmentShader = `
   uniform float time;
@@ -51,88 +51,93 @@ const fragmentShader = `
     vec3 color = mix(colorA, colorB, noiseValue);
     gl_FragColor = vec4(color, 1.0);
   }
-`;
+`
 
-const geometry = new THREE.SphereGeometry(10, 10, 10);
+const geometry = new THREE.SphereGeometry(10, 10, 10)
 const material = new THREE.ShaderMaterial({
-  vertexShader: vertexShader,
-  fragmentShader: fragmentShader,
+  vertexShader,
+  fragmentShader,
   uniforms: {
     time: { value: 0 },
     displacementScale: { value: 1 },
     colorA: { value: new THREE.Color(0x828282) },
     colorB: { value: new THREE.Color(0x383838) },
   },
-});
-const asteroid = new THREE.Mesh(geometry, material);
+})
+const asteroid = new THREE.Mesh(geometry, material)
 
 //--------------------------------------------------------------------------
 // Create a variable to keep track of the number of hits
-let numHits = 0;
+let numHits = 0
 
 // Function to handle the destruction of the asteroid
 function destroyAsteroid() {
   // Remove the asteroid from the scene
-  scene.remove(asteroid);
+  scene.remove(asteroid)
 }
 
 // Function to handle the asteroid getting hit
 function onAsteroidHit() {
   // Increment the number of hits
-  numHits++;
+  numHits++
 
   // If the asteroid has been hit 3 times, destroy it
   if (numHits >= 3) {
-    destroyAsteroid();
+    destroyAsteroid()
   }
 }
 // Example of how to trigger the onAsteroidHit() function when the asteroid gets hit
-asteroid.addEventListener('collision', onAsteroidHit);
+asteroid.addEventListener("collision", onAsteroidHit)
 //--------------------------------------------------------------------------
 
 // Define the number of clusters and the number of objects per cluster
-const numClusters = 5;
-const objectsPerCluster = 10;
+const numClusters = 5
+const objectsPerCluster = 10
 
 // Define the size of the area in which the clusters will be spawned
-const clusterAreaSize = 500;
+const clusterAreaSize = 500
 
 // Create a new group (asteroids) to hold the objects
-const asteroids = new THREE.Group();
+const asteroids = new THREE.Group()
 
 // Loop through the number of clusters
 for (let i = 0; i < numClusters; i++) {
   // Generate a random position for the cluster within the cluster area
-  const clusterX = Math.random() * clusterAreaSize - clusterAreaSize / 2;
-  const clusterY = Math.random() * clusterAreaSize - clusterAreaSize / 2;
-  const clusterZ = Math.random() * clusterAreaSize - clusterAreaSize / 2;
+  const clusterX = Math.random() * clusterAreaSize - clusterAreaSize / 2
+  const clusterY = Math.random() * clusterAreaSize - clusterAreaSize / 2
+  const clusterZ = Math.random() * clusterAreaSize - clusterAreaSize / 2
 
   // Create a new group to hold the objects in this cluster
-  const clusterGroup = new THREE.Group();
+  const clusterGroup = new THREE.Group()
 
   // Loop through the number of objects per cluster
   for (let j = 0; j < objectsPerCluster; j++) {
     // Generate a random position for the object within the cluster
-    const objectX = Math.random() * 100 - 5;
-    const objectY = Math.random() * 100 - 5;
-    const objectZ = Math.random() * 100 - 5;
+    const objectX = Math.random() * 100 - 5
+    const objectY = Math.random() * 100 - 5
+    const objectZ = Math.random() * 100 - 5
 
     // Create a new instance of the object
-    const instance = asteroid.clone();
+    const instance = asteroid.clone()
 
     // Set the position of the object relative to the position of the cluster
-    instance.position.set(clusterX + objectX, clusterY + objectY, clusterZ + objectZ);
-    //Roate the instance.
-    instance.rotation.x = (Math.random()-0.5)*10;
-    instance.rotation.y = (Math.random()-0.5)*10;
-    instance.rotation.z = (Math.random()-0.5)*10;
+    instance.position.set(
+      clusterX + objectX,
+      clusterY + objectY,
+      clusterZ + objectZ
+    )
+
+    //  Roate the instance
+    instance.rotation.x = (Math.random() - 0.5) * 10
+    instance.rotation.y = (Math.random() - 0.5) * 10
+    instance.rotation.z = (Math.random() - 0.5) * 10
 
     // Add the object to the cluster group
-    clusterGroup.add(instance);
+    clusterGroup.add(instance)
   }
 
   // Add the cluster group to the main group
-  asteroids.add(clusterGroup);
+  asteroids.add(clusterGroup)
 }
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------------
