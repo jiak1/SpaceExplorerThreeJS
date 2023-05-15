a86661169534fabaaf7bd78a21d46837abfd9b82
Michael Ning <crown.d@hotmail.com.au>
2023-05-15 17:09:00 +1000

    Fix master.

--- src/objects/objects.ts
+++ src/objects/objects.ts
@@ -1,6 +1,5 @@
 import { setupPlanets } from "./planet"
 import { setupShip } from "./ship"
-import { setupSkybox } from "./skybox"
 import { setupAsteroids } from "./asteroids"
 import { scene, objectsGroup } from "../renderer/renderer"
 import { setupExplosion } from "./explosion"
@@ -9,7 +8,6 @@ const setupObjects = () => {
   objectsGroup.clear()
   setupPlanets()
   setupShip()
-  setupSkybox()
   setupAsteroids()
   setupExplosion()
   scene.add(objectsGroup)
