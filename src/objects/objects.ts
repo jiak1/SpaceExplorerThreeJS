5ceb89b450c5734f570384f8a29a11a8577f7036
Michael Ning <crown.d@hotmail.com.au>
2023-05-15 17:10:52 +1000

    Revert "Fix master."
    This reverts commit a86661169534fabaaf7bd78a21d46837abfd9b82.

--- src/objects/objects.ts
+++ src/objects/objects.ts
@@ -1,5 +1,6 @@
 import { setupPlanets } from "./planet"
 import { setupShip } from "./ship"
+import { setupSkybox } from "./skybox"
 import { setupAsteroids } from "./asteroids"
 import { scene, objectsGroup } from "../renderer/renderer"
 import { setupExplosion } from "./explosion"
@@ -8,6 +9,7 @@ const setupObjects = () => {
   objectsGroup.clear()
   setupPlanets()
   setupShip()
+  setupSkybox()
   setupAsteroids()
   setupExplosion()
   scene.add(objectsGroup)
